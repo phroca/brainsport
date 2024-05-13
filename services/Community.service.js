4b4baf2430d647aee1a74499733b578c6869d192
ROCA Philippe <roca.philippe63@gmail.com>
2024-05-13 12:59:33 +0200

    BS : change ip address for services

--- services/Community.service.js
+++ services/Community.service.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const API = "http://ec2-13-38-100-228.eu-west-3.compute.amazonaws.com:3000";
+const API = "http://13.38.100.228:3000";
 //const API = "http://192.168.1.19:3000";
 
-const createGroup = async({title, colortheme, description, image="", userOwner, visibility}) => {
+const createGroup = async ({ title, colortheme, description, image = "", userOwner, visibility }) => {
     try {
-        return axios.put(`${API}/communitygroup`, {title, colortheme, description, image, userOwner, visibility})
+        return axios.put(`${API}/communitygroup`, { title, colortheme, description, image, userOwner, visibility })
     } catch (error) {
         console.error("Erreur à la création du groupe");
     }
 }
 
 /*Récupère les groupes validé */
-const getGroupsFromCurrentUser = async(userId) => {
+const getGroupsFromCurrentUser = async (userId) => {
     try {
         return axios.get(`${API}/users/${userId}/groups`)
     } catch (error) {
@@ -20,38 +20,38 @@ const getGroupsFromCurrentUser = async(userId) => {
     }
 }
 /*Récupère les groupes en attente */
-const getWaitingGroupsFromCurrentUser = async(userId) => {
+const getWaitingGroupsFromCurrentUser = async (userId) => {
     try {
         return axios.get(`${API}/users/${userId}/waitingGroups`)
     } catch (error) {
         console.error("Erreur à la récupération des groupe en attente de l'user " + userId);
     }
 }
-const getGroupsByIdFromCurrentUser = async(userId, groupId) => {
+const getGroupsByIdFromCurrentUser = async (userId, groupId) => {
     try {
         return axios.get(`${API}/users/${userId}/groups/${groupId}`)
     } catch (error) {
-        console.error("Erreur à la récupération du groupe "+ groupId + " de l'user " + userId);
+        console.error("Erreur à la récupération du groupe " + groupId + " de l'user " + userId);
     }
 }
 
-const getNbMemberOfGroup = async(groupId) => {
+const getNbMemberOfGroup = async (groupId) => {
     try {
         return axios.get(`${API}/communitygroup/${groupId}/nbMembers`)
     } catch (error) {
-        console.error("Erreur à la récupération du nombre d'utilisateur du groupe "+ groupId);
+        console.error("Erreur à la récupération du nombre d'utilisateur du groupe " + groupId);
     }
 }
 
-const getGroupUsersByGroupId = async(groupId) => {
+const getGroupUsersByGroupId = async (groupId) => {
     try {
         return axios.get(`${API}/communitygroup/${groupId}/users`)
     } catch (error) {
-        console.error("Erreur à la récupération des utilisateurs du groupe "+ groupId);
+        console.error("Erreur à la récupération des utilisateurs du groupe " + groupId);
     }
 }
 
-const getPublicGroupNotRegistered = async(userId) => {
+const getPublicGroupNotRegistered = async (userId) => {
     try {
         return axios.get(`${API}/communitygroup/user/${userId}/publicGroup`)
     } catch (error) {
@@ -59,37 +59,37 @@ const getPublicGroupNotRegistered = async(userId) => {
     }
 }
 
-const joinPublicGroup = async(groupId, userId) => {
+const joinPublicGroup = async (groupId, userId) => {
     try {
         return axios.put(`${API}/communitygroup/${groupId}/users/${userId}`)
     } catch (error) {
-        console.error("Erreur à rejoindre le groupe public "+ groupId);
+        console.error("Erreur à rejoindre le groupe public " + groupId);
     }
 }
-const joinPrivateGroup = async(groupId, userId) => {
+const joinPrivateGroup = async (groupId, userId) => {
     try {
         return axios.post(`${API}/communitygroup/privateGroup/${groupId}/users/${userId}`)
     } catch (error) {
-        console.error("Erreur à rejoindre le groupe privé "+ groupId);
+        console.error("Erreur à rejoindre le groupe privé " + groupId);
     }
 }
-const declineOrDeletePrivateGroup = async(groupUserId) => {
+const declineOrDeletePrivateGroup = async (groupUserId) => {
     try {
         return axios.delete(`${API}/communitygroup/deleteInvitation/${groupUserId}`)
     } catch (error) {
-        console.error("Erreur à la suppression de l'invitation du groupe "+ groupId);
+        console.error("Erreur à la suppression de l'invitation du groupe " + groupId);
     }
 }
 
-const inviteFriendToPrivateGroup = async(groupId, userId) => {
+const inviteFriendToPrivateGroup = async (groupId, userId) => {
     try {
         return axios.put(`${API}/communitygroup/${groupId}/add/${userId}`)
     } catch (error) {
-        console.error("Erreur à rejoindre le user" + userId + " au groupe privé "+ groupId);
+        console.error("Erreur à rejoindre le user" + userId + " au groupe privé " + groupId);
     }
 }
 
-const getGroupsById = async(groupId) => {
+const getGroupsById = async (groupId) => {
     try {
         return axios.get(`${API}/communitygroup/${groupId}`)
     } catch (error) {
@@ -97,16 +97,16 @@ const getGroupsById = async(groupId) => {
     }
 }
 
-const getGroupDiscussion = async(groupId) => {
+const getGroupDiscussion = async (groupId) => {
     try {
         return axios.get(`${API}/communitygroup/${groupId}/discussion`);
     } catch (error) {
         console.error("Erreur à la récupération de la discussion du groupe " + groupId);
     }
 }
-const sendMessageToGroupDiscussion = async({groupId, userId, message, dateEnvoi}) => {
+const sendMessageToGroupDiscussion = async ({ groupId, userId, message, dateEnvoi }) => {
     try {
-        return axios.put(`${API}/communitygroup/${groupId}/discussion`, {idUser: userId, message, dateEnvoi})
+        return axios.put(`${API}/communitygroup/${groupId}/discussion`, { idUser: userId, message, dateEnvoi })
     } catch (error) {
         console.error("Erreur à l'envoi du message de la discussion du groupe " + groupId);
     }
